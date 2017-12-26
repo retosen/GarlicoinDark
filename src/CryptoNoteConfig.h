60a488cd455868cb5e044e20796ed18191186ffa
retosen <34863491+retosen@users.noreply.github.com>
2017-12-26 21:21:40 +0000

    OSX Bugfixes
    Fixed bugs that were preventing compiling on OSX. Not sure what effects this will have on the behaviour of the program at large.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "garlicoindark";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210135e789a47736d322f3002f0a1fcb003a1f64818cfc4f74b662d649caeafbcd3f";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
